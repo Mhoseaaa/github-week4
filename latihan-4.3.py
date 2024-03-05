ac2a0d5956aec8eeb9eeecc3de3778c441203ec8
MIchael Hosea <michael.hosea@ti.ukdw.ac.id>
2024-03-05 23:46:13 +0700

    finaleee

--- latihan-4.3.py
+++ latihan-4.3.py
@@ -4,21 +4,17 @@ celcius_to_fahrenheit = lambda C: (9/5) * C + 32
 # Fungsi konversi Celcius ke Reamur
 celcius_to_reamur = lambda C: 0.8 * C
 
-# Meminta input dari user
-choice = input("Pilih konversi:\n1. Celsius ke Fahrenheit\n2. Celsius ke Reamur\n")
+# Test-case 1
+C = 100.0
+F = celcius_to_fahrenheit(C)
+print("Input C =", C, ". Output F =", F)
 
-# Validasi pilihan
-if choice == "1":
-    
-    # Konversi Celsius ke Fahrenheit
-    C = float(input("Masukkan suhu dalam Celsius: "))
-    F = celcius_to_fahrenheit(C)
-    print("Input C =", C, ". Output F =", F)
-    
-elif choice == "2":
-    # Konversi Celsius ke Reamur
-    C = float(input("Masukkan suhu dalam Celsius: "))
-    R = celcius_to_reamur(C)
-    print("Input C =", C, ". Output R =", R)
-else:
-    print("Pilihan tidak valid")
+# Test-case 2
+C = 80.0
+R = celcius_to_reamur(C)
+print("Input C =", C, ". Output R =", R)
+
+# Test-case 3
+C = 0.0
+F = celcius_to_fahrenheit(C)
+print("Input C =", C, ". Output F =", F)
